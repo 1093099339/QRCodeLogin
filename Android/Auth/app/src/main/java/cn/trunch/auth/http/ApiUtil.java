0d1ccec25aab019b819c2bb25b7f71d410e27b1b
liuya <1093099339@qq,com>
2021-07-09 11:47:56 +0800

    本地运行

--- Android/Auth/app/src/main/java/cn/trunch/auth/http/ApiUtil.java
+++ Android/Auth/app/src/main/java/cn/trunch/auth/http/ApiUtil.java
@@ -8,7 +8,8 @@ public class ApiUtil {
     public static String USER_NAME = ""; // 用户昵称
 
 
-    public final static String IP_PORT = "http://192.168.137.1/";
+    //public final static String IP_PORT = "http://192.168.137.1/";
+    public final static String IP_PORT = "http://192.168.119.106/";
     public final static String LOGIN = IP_PORT + "login";
     public final static String TOKEN_INFO = IP_PORT + "auth/info/";
     public final static String TOKEN_USE = IP_PORT + "auth/use/";
